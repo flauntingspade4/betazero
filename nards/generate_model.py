4bf0562710132987a201cef24d7513de3504acfd
flauntingspade4 <elliotwhybrow@gmail.com>
2025-03-22 04:04:13 +0000

    Change learning rate and threshold

--- nards/generate_model.py
+++ nards/generate_model.py
@@ -65,7 +65,7 @@ if __name__ == "__main__":
     encoder.trainable = False
     
     n_model = NardsModel(encoder)
-    optimizer = keras.optimizers.Adam(1e-5)
-    accuracy_metric = keras.metrics.BinaryAccuracy(threshold=0.9)
+    optimizer = keras.optimizers.Adam(0.01)
+    accuracy_metric = keras.metrics.BinaryAccuracy(threshold=0.5)
     n_model.compile(optimizer, loss="categorical_crossentropy", metrics=[accuracy_metric, "binary_crossentropy", "mae"])
     train_model(n_model)
