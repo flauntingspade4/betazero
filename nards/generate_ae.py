c97ddf631eb7e0298e960ddfa1c01f2082ec1b54
elliot <elliotwhybrow@gmail.com>
2025-03-26 16:34:57 +0000

    Make the model actually work (Somehow)

--- nards/generate_ae.py
+++ nards/generate_ae.py
@@ -3,14 +3,15 @@ import tf_keras as keras
 import pickle
 
 
-FILTERS = [128, 64, 32]
+FILTERS = [600, 400, 200]
 
 LATENT_DIM = 100
 
 class Encoder(keras.Model):
     def __init__(self):
         super(Encoder, self).__init__()
-        self.conv_layers = [keras.layers.Conv2D(x, (3, 3), padding="same", activation="relu") for x in FILTERS]
+        # self.conv_layers = [keras.layers.Conv2D(x, (3, 3), padding="same", activation="relu") for x in FILTERS]
+        self.conv_layers = [keras.layers.Dense(x, activation="relu") for x in FILTERS]
         self.flatten = keras.layers.Flatten()
         self.latent = keras.layers.Dense(LATENT_DIM, activation="relu")
 
@@ -29,17 +30,19 @@ class Decoder(keras.Model):
         self.dense_input = keras.layers.Dense(8 * 8 * FILTERS[-1])
         included_layers = FILTERS.copy()
         included_layers.reverse()
-        self.conv_layers = [keras.layers.Conv2D(x, (3, 3), padding="same", activation="relu") for x in included_layers]
-        self.output_layer = keras.layers.Conv2D(12, (3, 3), padding="same", activation="sigmoid")
+        # self.conv_layers = [keras.layers.Conv2D(x, (3, 3), padding="same", activation="relu") for x in included_layers]
+        self.conv_layers = [keras.layers.Dense(x, activation="relu") for x in included_layers]
+        # self.output_layer = keras.layers.Conv2D(12, (3, 3), padding="same", activation="sigmoid")
+        self.output_layer = keras.layers.Dense(8 * 8 * 12, activation="sigmoid")
 
 
     @tf.function
     def call(self, x):
         x = self.dense_input(x)
-        x = tf.reshape(x, (-1, 8, 8, FILTERS[-1]))
+        # x = tf.reshape(x, (-1, 8, 8, FILTERS[-1]))
         for layer in self.conv_layers:
             x = layer(x)
-        return self.output_layer(x)
+        return tf.reshape(self.output_layer(x), (-1, 8, 8, 12))
 
 
 class AutoEncoder(keras.Model):
@@ -58,8 +61,8 @@ class AutoEncoder(keras.Model):
 def generate_model():
     model = AutoEncoder()
     optimizer = keras.optimizers.Adam(learning_rate=0.00005)
-    accuracy_metric = keras.metrics.BinaryAccuracy(threshold=0.9)
-    model.compile(optimizer, loss="binary_crossentropy", metrics=[accuracy_metric, "mse"])
+    accuracy_metric = keras.metrics.BinaryAccuracy(threshold=0.5)
+    model.compile(optimizer, loss="binary_crossentropy", metrics=[accuracy_metric, "mse", "mae"])
     return model
     
 
@@ -70,15 +73,21 @@ def prepare_moves(moves):
 
 
 def train_model(model):
-    with open("latest.pickle", "rb") as f:
-        moves = pickle.load(f)
-        generator = lambda: prepare_moves(moves)
+    moves = []
+    for name in ["white_won", "white_lost", "black_won", "black_lost"]:
+        n_moves = pickle.load(open(name, "rb"))
+        for m in n_moves:
+            moves.append(m)
+            if len(n_moves) > 800_000:
+                break
+    print("{} total moves".format(len(moves)))
+    generator = lambda: prepare_moves(moves)
     output_signature = (tf.TensorSpec(shape=(8, 8, 12), dtype=tf.float32), tf.TensorSpec(shape=(8, 8, 12), dtype=tf.float32))
     dataset = tf.data.Dataset.from_generator(generator, output_signature=output_signature).shuffle(100000).batch(16)
-    train_dataset, test_dataset = tf.keras.utils.split_dataset(dataset, left_size=0.9)
-    model.fit(train_dataset, epochs=5)
+    # train_dataset, test_dataset = tf.keras.utils.split_dataset(dataset, left_size=0.9)
+    model.fit(dataset, epochs=5)
     
-    model.evaluate(test_dataset)
+    # model.evaluate(test_dataset)
     
     input_spec = tf.TensorSpec(shape=(None, 8, 8, 12), dtype=tf.float32)
     signatures = { "call": model.call.get_concrete_function(input_spec) }
--- nards/generate_model.py
+++ nards/generate_model.py
@@ -5,114 +5,108 @@ import generate_ae
 import sys
 import random
 
-FILTERS = [256, 128, 64, 32]
-UNITS = [200, 100, 32]
+UNITS = [256, 128, 64]
+leaky = keras.layers.LeakyReLU()
+L2_REGULARIZATION = 0.0001
 
 
 class NardsModel(keras.Model):
-    def __init__(self, encoder):
+    def __init__(self):
         super(NardsModel, self).__init__()
-        self.encoder = encoder
+        # self.encoder = encoder
         self.feature_extraction = []
-        for unit in [128, 64]:
-            self.feature_extraction.append(keras.layers.Dense(unit, activation="relu"))
-            self.feature_extraction.append(keras.layers.Dropout(0.2))
+        for unit in [256, 256, 128]:
+            self.feature_extraction.append(keras.layers.Conv2D(unit, (3, 3), padding="same", activation=leaky, kernel_regularizer=keras.regularizers.L2(L2_REGULARIZATION)))
+            # self.feature_extraction.append(keras.layers.Dropout(0.2))
         self.concat = keras.layers.Concatenate()
+        self.flatten = keras.layers.Flatten()
         # self.dense_input = keras.layers.Dense(8 * 8 * generate_ae.FILTERS[-1])
         self.dense_and_dropout = []
         for unit in UNITS:
-            self.dense_and_dropout.append(keras.layers.Dense(unit, activation="relu"))
+            self.dense_and_dropout.append(keras.layers.Dense(unit, activation=leaky, kernel_regularizer=keras.regularizers.L2(L2_REGULARIZATION)))
             self.dense_and_dropout.append(keras.layers.Dropout(0.2))
         # self.dropout_input = keras.layers.Dropout(0.2)
         # self.conv_layers = [keras.layers.Conv2D(x, (3, 3), padding="same", activation="relu", kernel_regularizer=keras.regularizers.L2(0.0001)) for x in FILTERS]
-        # self.flatten = keras.layers.Flatten()
-        # self.dropout_output = keras.layers.Dropout(0.2)
-        self.output_layer = keras.layers.Dense(2, activation="sigmoid")
+        self.output_layer = keras.layers.Dense(2, activation="softmax")
 
     @tf.function()
-    def call(self, lhs, rhs):
-        lhs = self.encoder(lhs)
+    def call(self, inputs):
+        lhs, rhs = inputs
         for layer in self.feature_extraction:
             lhs = layer(lhs)
-        rhs = self.encoder(rhs)
-        for layer in self.feature_extraction:
             rhs = layer(rhs)
         x = self.concat([lhs, rhs])
+        x = self.flatten(x)
         # x = self.dense_input(x)
         # x = self.dropout_input(x)
         # Reshape from the latent space
-        # x = tf.reshape(x, (-1, 8, 8, generate_ae.FILTERS[-1]))
         for layer in self.dense_and_dropout:
             x = layer(x)
-        # x = self.dropout_output(x)
-        # x = self.flatten(x)
         return self.output_layer(x)
 
 
-def prepare_moves(moves):
-    for move in moves:
-        input = tf.reshape(tf.constant(move["board"]["data"]), (8, 8, 12))
-        output = tf.constant(move["result"])
-        yield input, output
-        
-
-def prepare_file(file_name):
-    parsed = []
-    data = pickle.load(open(file_name, "rb"))
-    for position in data:
-        parsed.append(tf.reshape(tf.constant(position["board"]["data"]), (8, 8, 12)))
-    return tf.convert_to_tensor(parsed)
+def prepare_file(file_name, parsed):
+    with open(file_name, "rb") as f:
+        data = pickle.load(f)
+        print("Doing file with {} entries".format(len(data)))
+        for i, position in enumerate(data):
+            parsed.append(tf.reshape(tf.constant(position["board"]["data"], dtype=tf.float32), (8, 8, 12)))
+            if i > 200_000:
+                break
+        print("Done file")
 
 
-def data_generator(self):
+def data_generator(files):
     while True:
-        first_index = random.randint(0, 3)
-        if first_index > 1:
-            second_index = first_index - 2
+        first_index = random.randint(0, 1)
+        second_index = 1 - first_index
+        if first_index == 0:
+            won = tf.constant([1.0, 0.0], dtype=tf.float32)
         else:
-            second_index = first_index + 2
-        if first_index == 0 or first_index == 3:
-            won = tf.constant([1, 0])
-        else:
-            won = tf.constant([0, 1])
-        lhs_game_index = random.randint(0, len(self.files[first_index] - 1))
-        rhs_game_index = random.randint(0, len(self.files[second_index] - 1))
-        yield (self.files[first_index][lhs_game_index], self.files[second_index][rhs_game_index]), won
+            won = tf.constant([0.0, 1.0], dtype=tf.float32)
+
+        lhs_game_index = random.randrange(len(files[first_index]))
+        rhs_game_index = random.randrange(len(files[second_index]))
+        
+        yield (files[first_index][lhs_game_index], files[second_index][rhs_game_index]), won
 
 
 def train_model(model):
-    filenames = ["white_won", "white_lost", "black_lost", "black_won"]
-    files = [prepare_file(p) for p in filenames]
+    won_games = []
+    lost_games = []
+    for w_name in ["white_won", "black_won"]:
+        prepare_file(w_name, won_games)
+    for l_name in ["white_lost", "black_lost"]:
+        prepare_file(l_name, lost_games)
+    files = [won_games, lost_games]
+    # print("Output: {}".format(model(tf.constant([[won_games[0]], [lost_games[0]]]))))
 
-    # with open("latest.pickle", "rb") as f:
-        # moves = pickle.load(f)
-        # generator = lambda: prepare_moves(moves)
     output_signature = ((tf.TensorSpec(shape=(8, 8, 12), dtype=tf.float32), tf.TensorSpec(shape=(8, 8, 12), dtype=tf.float32)), tf.TensorSpec(shape=(2,), dtype=tf.float32))
-    # dataset = tf.data.Dataset.from_generator(generator, output_signature=output_signature).shuffle(100000).batch(16)
-    dataset = tf.data.Dataset.from_generator(data_generator, output_signature=output_signature, args=files).take(100_000).batch(16)
-    train_dataset, test_dataset = tf.keras.utils.split_dataset(dataset, left_size=0.9)
-    model.fit(train_dataset, epochs=5)
-    
-    model.evaluate(test_dataset)
-    
-    input_spec = (tf.TensorSpec(shape=(None, 8, 8, 12), dtype=tf.float32), tf.TensorSpec(shape=(None, 8, 8, 12), dtype=tf.float32))
-    signatures = { "call": model.call.get_concrete_function(input_spec) }
-    model.save("ae_model", save_format="tf", signatures=signatures)
+    dataset = tf.data.Dataset.from_generator(data_generator, output_signature=output_signature, args=[files]).take(100_000).batch(32).prefetch(tf.data.AUTOTUNE)
+    # train_dataset, test_dataset = tf.keras.utils.split_dataset(dataset, left_size=0.9)
+    for _ in range(10):
+        print("Calling fit")
+        model.fit(dataset, epochs=100)
+        
+        # test_dataset = tf.data.Dataset.from_generator(data_generator, output_signature=output_signature, args=[files]).take(100_000).batch(16)
+        # model.evaluate(test_dataset)
 
+        input_spec = (tf.TensorSpec(shape=(None, 8, 8, 12), dtype=tf.float32), tf.TensorSpec(shape=(None, 8, 8, 12), dtype=tf.float32))
+        signatures = { "call": model.call.get_concrete_function(input_spec) }
+        model.save("model", save_format="tf", signatures=signatures)
 
-# seq = PositionSequence()
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "new":
-        model = generate_ae.generate_model()
-        generate_ae.train_model(model)
-        encoder = model.encoder
-    else:
-        encoder = keras.models.load_model("enc_model")
-    encoder.trainable = False
+    # if len(sys.argv) > 1 and sys.argv[1] == "new":
+        # model = generate_ae.generate_model()
+        # generate_ae.train_model(model)
+        # encoder = model.encoder
+    # else:
+        # encoder = keras.models.load_model("enc_model")
+    # encoder.trainable = False
     
-    n_model = NardsModel(encoder)
-    optimizer = keras.optimizers.Adam(0.001)
+    n_model = NardsModel()
+    optimizer = keras.optimizers.Adam(0.0005)
     accuracy_metric = keras.metrics.BinaryAccuracy(threshold=0.5)
-    n_model.compile(optimizer, loss="binary_crossentropy", metrics=[accuracy_metric, "mae"])
+    n_model.compile(optimizer, loss="categorical_crossentropy", metrics=[accuracy_metric, "accuracy", "mae", "mse"])
     train_model(n_model)
